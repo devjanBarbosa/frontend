b5f4976bd7bfd62cfd64a7b7b1270af230ee1af3
janluiz <jan_2020@hotmail.com>
2025-09-06 12:02:00 -0300

    MUDANÇA NO PIX E SEO

--- src/app/services/seo.ts
+++ src/app/services/seo.ts
@@ -140,8 +140,8 @@ export class SeoService {
   }
 
   private updateTagsForStaticPage(url: string): void {
-    // Títulos e descrições continuam os mesmos
-    let title = 'Leda Cosméticos | Entrega Rápida em Bangu e Região, RJ';
+  
+    let title = 'Leda Cosméticos | Entrega Rápida em toda a Zona Oeste, RJ';
     let description = 'Descubra nossa seleção de cosméticos e presentes em Bangu, com entrega rápida. A curadoria especial da Leda para a sua beleza.';
 
     switch (url) {
